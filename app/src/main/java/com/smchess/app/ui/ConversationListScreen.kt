8282b112bf7dd19a01226cd522bf71049db215fe
luminard8 <luminard8@gmail.com>
2026-06-23 22:52:03 +0200

    Update ConversationListScreen.kt

--- app/src/main/java/com/smchess/app/ui/ConversationListScreen.kt
+++ app/src/main/java/com/smchess/app/ui/ConversationListScreen.kt
@@ -21,6 +21,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -36,6 +37,7 @@ data class ConversationDisplay(
     val gamePhase: GamePhase
 )
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ConversationListScreen(
     conversations: List<ConversationDisplay>,
